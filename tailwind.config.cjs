61a863533b4ccffc3f8cd7e889b338039a14c8d6
augustCaio <caioaugusto930@gmail.com>
2023-02-15 15:58:20 -0300

    tech stack

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,10 @@ module.exports = {
 				'dark-blue': '#2e313e',
 				'light-blue': '#66CAF2',
 				'medium-blue': '#368ABF',
-				'off-white': '#D9D9D9'
+				'off-white': '#D9D9D9',
+				'html-orange': '#FA683C',
+				'css-blue': '#254BDD',
+				'tailwind-blue': '#38BDF8',
 			},
 			fontFamily: {
 				sans: "Montserrat, sans-serif",
