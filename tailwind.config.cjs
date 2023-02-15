23a5d12f3d0dce169e9564b2cbacb31b53a3f600
augustCaio <caioaugusto930@gmail.com>
2023-02-15 10:22:38 -0300

    footer desenvolvido

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
 				'dark-blue': '#2e313e',
 				'light-blue': '#66CAF2',
 				'medium-blue': '#368ABF',
+				'off-white': '#D9D9D9'
 			},
 			fontFamily: {
 				sans: "Montserrat, sans-serif",
